016602e69309afa1ff0548607d1f6fbee3bfc830
abderrahim <abderrahim581@hotmail.com>
2021-03-27 18:38:27 +0100

    fix the problem of send email

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -33,7 +33,7 @@ const ContactForm = () => {
     };
 
     if (name && isEmail() && message) {
-      const templateId = "template_aofmtvBG";
+      const templateId = "template_r0qjgd8";
 
       nameS.classList.remove("red");
       emailS.classList.remove("red");
--- src/pages/About.js
+++ src/pages/About.js
@@ -6,8 +6,6 @@ import DynamicText from "../components/DynamicText";
 import Mouse from "../components/Mouse";
 import { motion } from "framer-motion";
 function About() {
-    
-
   const variants = {
     initial: {
       opacity: 0,
@@ -24,9 +22,10 @@ function About() {
       x: -100,
     },
   };
-  return(
-       <main>
+  return (
+    <main>
       <Mouse />
+
       <motion.div
         className="home"
         initial="initial"
@@ -36,13 +35,45 @@ function About() {
       >
         <Navigation />
         <SocialNetwork />
-        <div className='about-main'>
-        <div className='about-content' >
-        <h1>About</h1>
-        <h2> je suis abderrahim ait chraa j'ai 23 ans </h2>
+        <div className="about-main">
+          <div className="about-content">
+            <div className="image__about">
+              <img className="image" alt="" src="./assets/img/perfil.png" />
+
+              <div>
+                <h1>About</h1>
+                <h2>
+                  Hey ! my name is abderrahim ait chraa i have 23 years old and
+                  i'm junior web developer
+                </h2>
+              </div>
+              <section class="skills section" id="skills">
+                <h2 class="section-title">Skills</h2>
+
+                <div class="skills__container bd-grid">
+                  <div class="skills__box">
+                    <h3 class="skills__subtitle">Development</h3>
+                    <span class="skills__name">Html</span>
+                    <span class="skills__name">Css</span>
+                    <span class="skills__name">Javascript</span>
+                    <span class="skills__name">Scss</span>
+                    <span class="skills__name">React</span>
+                    <span class="skills__name">Node</span>
+
+                    <h3 class="skills__subtitle">Design</h3>
+                    <span class="skills__name">Figma</span>
+                    <span class="skills__name">Affinity</span>
+                  </div>
+
+                  <div class="skills__img">
+                    <img src="assets/img/skill.jpg" alt="" />
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
         </div>
-        </div>
-      
+        <ButtonsBottom />
       </motion.div>
     </main>
   );
